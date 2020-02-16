4ac1f4926654ad83538569e3af36b1d5d8a7f71c
NeedWolf <cogansj2@gmail.com>
2020-02-16 13:51:33 -0500

    Input and Output functions are implemented!

--- FileIO.py
+++ FileIO.py
@@ -2,10 +2,6 @@
 # Each row in the file is an element in a list
 # The list is returned
 
-import sys
-from typing import List, Any
-
-
 class FileIO:
     # Reads a file
     def input(file):
@@ -16,8 +12,21 @@ class FileIO:
             s = file.readline()
         file.close()
         return l
-    def output(dict):
+    def output(students):
+        # inputs a lis with the following format
+        # [["Sam", 87.2, 'B'], ["Daniel", 62.0, 'D']]
         output = open("studentGradesOutput.txt", 'w')
+        for student in students:
+            i = 0
+            for x in student:
+                if i == 0:
+                    output.write(" " * (20 - len(student[i])) + student[i])
+                else:
+                    output.write(" " + str(student[i]))
+                if i == 2:
+                    output.write("\n")
+                i += 1
+        output.close()
 
 
 
