22719884f33df932b5e2ec3229f1cf4aa0bc6b1d
pauljr2 <60938424+pauljr2@users.noreply.github.com>
2020-02-25 18:23:52 -0500

    Update LibraryDict.py

--- Hw2/LibraryDict.py
+++ Hw2/LibraryDict.py
@@ -1,5 +1,6 @@
-# Scotts Methods
+# Names
 
+# Format author names
 def formatNames(message):
     nameList = message.split(", ")
     endString = ""
@@ -13,54 +14,37 @@ def formatNames(message):
         i = i + 1
     return endString
 
-    # This method takes in an entry
-
-"""
-This formats a muti-line string. For example,
-
-Journal
-Key: Kiper1996
-Author: James Kiper
-Title: Criteria for Evaluation of Visual Programming Languages
-Journal: Journal of Visual Languages & Computing
-Publisher: Elsevier
-Date: 1996
-Volume: 8
-Number: 2
-
-"""
-
+# Prepare values for formatting, and establish the key for the specified Journal, Book, or Conference
 def getKeyValue(string):
-    # Puts each line in list
+    # Inputs values all together
     lines = string.split("\n")
-    # setup to return a key and value
+    # Initializes the key
     result = [str.replace(lines[1], "Key: ", "", 1), ""]
-    # Is it a book, journal etc.
+    # Determines type of reference (Book, Journal, Conference)
     type = lines[0]
-    # Gets the names in the formated way
+    # Grabs formatted author names
     authors = formatNames(str.replace(lines[2], "Author: ", "", 1))
     # Gets the title without the "Title: " part
     title = str.replace(lines[3], "Title: ", "", 1)
     # Creates the first part of every type of media citation
     result[1] = result[1] + authors + ", " + title
 
-    # Journal Case:
+    # Add formatted Journal data
     if lines[0] == "Journal":
         # result[1] is the string or the value of the dictionary
         result[1] = result[1] + JournalHelper(lines)
 
-    # Book Case:
+    # Add formatted Book data
     elif lines[0] == "Book":
         result[1] = result[1] + BookHelper(lines)
 
-    # Conference Case:
+    # Add formatted Conference data
     elif lines[0] == "Conference":
         result[1] = result[1] + ConferenceHelper(lines)
 
     return result
 
-
-# Returns the second part of the string in the dictionary value
+# Format Journal data
 def JournalHelper(list):
     string = ""
     # Gets the values without the leading "...: "
@@ -73,7 +57,7 @@ def JournalHelper(list):
     string = ", " + journal + ", " + publisher + ":" + volume + "(" + number + "), " + date
     return string
 
-#Returns the second part of a book string as dictionary value
+# Format Book data
 def BookHelper(list):
     publisher = str.replace(list[4], "Publisher: ", "", 1)
     date = str.replace(list[5], "Date: ", "", 1)
@@ -81,6 +65,7 @@ def BookHelper(list):
     bstring = ", " + publisher + ", " + date + "."
     return bstring
 
+# Format Conference data
 def ConferenceHelper(list):
     conference = str.replace(list[4], "Conference: ", "", 1)
     date = str.replace(list[6], "Date: ", "", 1)
@@ -90,32 +75,34 @@ def ConferenceHelper(list):
     cstring = ", in " + conference + ", " + date + ", " + location + ", " + pages + "."
     return cstring
 
-
+# This is the actual dictionary object which holds all of the library data.
 dict = {}
-
 
 book_str = ""
 journal_str = ""
 conference_str = ""
 
-
+#Open the file and add in dictionary values.
 with open("Step3Data.txt") as fp:
     for line in fp:
-
+        
+        # Add a Book to the dictionary
         if line == "Book\n":
             book_str += line
             for i in range(0,5):
                 book_str += next(fp)
             dict[getKeyValue(book_str)[0]] = getKeyValue(book_str)[1]
             book_str = ""
-
+        
+        # Add a Journal to the dictionary
         if line == "Journal\n":
             journal_str += line
             for i in range(0,8):
                 journal_str += next(fp)
             dict[getKeyValue(journal_str)[0]] = getKeyValue(journal_str)[1]
             journal_str = ""
-
+            
+        # Add a Conference to the dictionary
         if line == "Conference\n":
             conference_str += line
             for i in range(0,7):
@@ -123,7 +110,7 @@ with open("Step3Data.txt") as fp:
             dict[getKeyValue(conference_str)[0]] = getKeyValue(conference_str)[1]
             conference_str = ""
 
-
+# Prompt user for a key, and return data if that key exists
 print("Please enter key: ")
 input_key = input()
 
