ac22a1ba9ae0c38421a982595eeb4378fa240852
Andrew Grimes <49619879+DrewGrimes20@users.noreply.github.com>
2020-04-09 12:36:30 -0400

    Add files via upload

--- Exercise 04/Calculator.java
+++ Exercise 04/Calculator.java
@@ -1,28 +1,35 @@
-package Q1;
+
 public class Calculator{
 
 	private static int result; 
+
 	public void add(int n){
 		result = result + n;
-		}
+	}
 	public void substract(int n){
-		result = result - 1; 
-		}
+		result = result - n; 
+	}
 	public void multiply(int n){
-		
+		result *= n;
 	} 
 	public void divide(int n){
 		result = result / n;
-		}
+	}
 	public void square(int n){
 		result = n * n;
-		}
+	}
 	public void squareRoot(int n){
-		for (; ;) ;
+		result = n;
+		int squareRoot = n/2;
+		for (int temp = squareRoot * 2; temp-squareRoot !=0; squareRoot = (temp + (n / temp))/2) {
+			temp = squareRoot;
+			result = temp;
 		}
+
+	}
 	public void clear(){ 
 		result = 0;
-		}
+	}
 	public int getResult(){
 		return result;
 	}
