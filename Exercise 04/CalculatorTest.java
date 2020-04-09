1c84872482680e0e32804436ae6fb3bce26bfca5
Andrew Grimes <49619879+DrewGrimes20@users.noreply.github.com>
2020-04-09 12:35:06 -0400

    Add files via upload

--- Exercise 04/CalculatorTest.java
+++ Exercise 04/CalculatorTest.java
@@ -36,77 +36,77 @@ public class CalculatorTest {
 		assertTrue(calculator.getResult() == 0);
 	}
 
-	
+
 	//@Ignore("Multiply() Not yet implemented")
-		@Test
-		public void testMultiply() {
-			calculator.add(3);
-			calculator.multiply(1);
-			assertEquals(3, calculator.getResult());
-			calculator.clear();
-			
-			calculator.add(0);
-			calculator.multiply(1);
-			assertEquals(0, calculator.getResult());
-			calculator.clear();
-			
-			calculator.add(3);
-			calculator.multiply(1);
-			assertEquals(3, calculator.getResult());
-			calculator.clear();
-			
-			calculator.add(7);
-			calculator.multiply(4);
-			assertEquals(28, calculator.getResult());
-			calculator.clear();
-			
-			calculator.add(4);
-			calculator.multiply(2);
-			assertEquals(8, calculator.getResult());
-		}
+	@Test
+	public void testMultiply() {
+		calculator.add(3);
+		calculator.multiply(1);
+		assertEquals(3, calculator.getResult());
+		calculator.clear();
 
-		@Test
-		public void testDivide() {
-			//add code here
-			calculator.add(3);
-			calculator.divide(3);
-			assertEquals(1, calculator.getResult());
-			calculator.clear();
-			
-			calculator.add(0);
-			calculator.divide(1);
-			assertEquals(0, calculator.getResult());
-			calculator.clear();
-			
-			calculator.add(7);
-			calculator.divide(4);
-			assertEquals(7/4, calculator.getResult());
-			calculator.clear();
-			
-			calculator.add(8);
-			calculator.divide(2);
-			assertEquals(4, calculator.getResult());
-		}
-    
-    @Test
-    public void testSquare() {
-        calculator.square(2);
-        assertTrue(calculator.getResult() == 4);
-        calculator.square(4);
-        assertTrue(calculator.getResult()==16);
-        calculator.square(0);
-        assertTrue(calculator.getResult()==0);
-        calculator.square(-2);
-        assertTrue(calculator.getResult()==4);
-    }
+		calculator.add(0);
+		calculator.multiply(1);
+		assertEquals(0, calculator.getResult());
+		calculator.clear();
 
-    @Test
-    public void testSquareRoot() {
-    	calculator.squareRoot(16);
-    	assertTrue(calculator.getResult()==4);
-    	calculator.squareRoot(36);
-    	assertTrue(calculator.getResult()==6);
-    	calculator.squareRoot(0);
-    	assertTrue(calculator.getResult()==0);
-    }
+		calculator.add(3);
+		calculator.multiply(1);
+		assertEquals(3, calculator.getResult());
+		calculator.clear();
+
+		calculator.add(7);
+		calculator.multiply(4);
+		assertEquals(28, calculator.getResult());
+		calculator.clear();
+
+		calculator.add(4);
+		calculator.multiply(2);
+		assertEquals(8, calculator.getResult());
+	}
+
+	@Test
+	public void testDivide() {
+		//add code here
+		calculator.add(3);
+		calculator.divide(3);
+		assertEquals(1, calculator.getResult());
+		calculator.clear();
+
+		calculator.add(0);
+		calculator.divide(1);
+		assertEquals(0, calculator.getResult());
+		calculator.clear();
+
+		calculator.add(7);
+		calculator.divide(4);
+		assertEquals(7/4, calculator.getResult());
+		calculator.clear();
+
+		calculator.add(8);
+		calculator.divide(2);
+		assertEquals(4, calculator.getResult());
+	}
+
+	@Test
+	public void testSquare() {
+		calculator.square(2);
+		assertTrue(calculator.getResult() == 4);
+		calculator.square(4);
+		assertTrue(calculator.getResult()==16);
+		calculator.square(0);
+		assertTrue(calculator.getResult()==0);
+		calculator.square(-2);
+		assertTrue(calculator.getResult()==4);
+	}
+
+	@Test
+	public void testSquareRoot() {
+		calculator.squareRoot(16);
+		assertTrue(calculator.getResult()==4);
+		calculator.squareRoot(36);
+		assertTrue(calculator.getResult()==6);
+		calculator.squareRoot(0);
+		assertTrue(calculator.getResult()==0);
+	}
 }
