a847b771e6b6ca2680ec7c2eae586ee0dc9fa180
NeedWolf <cogansj2@gmail.com>
2020-04-16 17:43:10 -0400

    Language is finished, but doesn't draw

--- HW4/TurtleSoup2.java
+++ HW4/TurtleSoup2.java
@@ -8,6 +8,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.ObjectInputStream.GetField;
 import java.lang.Math;
 import java.util.List;
 import java.util.Scanner;
@@ -21,9 +22,12 @@ public class TurtleSoup2 {
 	private static Stack<String> begins;
 	private static StringTokenizer tokens;
 	private static DrawableTurtle turtle;
+	private static int tokenCounter = 0;
+	private static String lastTokenEver = "";
+	private static String lastVariable = "";
     
-	private static Integer nextInt() {
-		String variable = tokens.nextToken();
+	private static Integer nextInt(StringTokenizer blockTokens) {
+		String variable = nextToken(blockTokens);
 		Integer resultingInteger = null;
 		if (variables.containsKey(variable)) {
 			resultingInteger = variables.get(variable);
@@ -34,51 +38,79 @@ public class TurtleSoup2 {
 		return resultingInteger;
 	}
 	
-	private static Double nextDouble() {
-		return new Double(nextInt());
+	private static Integer nextInt() {
+		return nextInt(tokens);
 	}
-
-    
-	private static String runToken(String token) {
+	
+	private static Double nextDouble(StringTokenizer blockTokens) {
+		return new Double(nextInt(blockTokens));
+	}
+	
+	private static String nextToken(StringTokenizer blockTokens){
+		return blockTokens.nextToken();
+	}
+	
+	private static String nextToken() {
+		tokenCounter++;
+		lastTokenEver = nextToken(tokens);
+		return lastTokenEver;
+	}
+	
+	private static String getBlockTokens(String beginToken) { // had to create this since the while loop caused problems when the while loop happened twice
+		String blockTokensStr = beginToken;
+		String token = beginToken;
+		while (!token.equals("end")) {
+			token = nextToken();
+			blockTokensStr = blockTokensStr + " " + token;
+		}
+		return blockTokensStr;
+	}
+	
+	private static String runToken(String token, StringTokenizer blockTokens) {
     	switch(token){
     	case "begin": 
     		begins.add(token);
     		break;
     	case "end":
     		begins.pop();
+    		break;
     	case "loop":
-    		for (int i = 1; i <= nextInt(); i++){
-    			String lastToken = "";
-    			while (lastToken != "end") {
-    				lastToken = runToken(tokens.nextToken());
+    		int count = nextInt();
+    		String beginToken = nextToken();
+    		String loopTokens = getBlockTokens(beginToken);
+    		for (int i = 1; i <= count; i++){
+    			StringTokenizer loopTokenizer = new StringTokenizer(loopTokens);
+    			String loopToken = "";
+    			while (!loopToken.equals("end")) {
+    				loopToken = runToken(loopTokenizer.nextToken(), loopTokenizer);
+    				System.out.println("LoopToken: " + loopToken);
     			}
+    			System.out.println("Loop" + i);
     		}
     		break;
     	case "forward":
-			turtle.forward(nextInt());
+			turtle.forward(nextInt(blockTokens));
 			break;
 		
 		case "turn":
-			turtle.turn(nextDouble());
+			turtle.turn(nextDouble(blockTokens));
 			break;
 		case "programEnd":
 			break;
 		case "=":
+			String variableName = lastVariable;
+			variables.put(variableName, nextInt());
 			break;
     	default: // Gets wrong tokens (nextToken is giving problems)
-    		String variableName = token;
-    		String nextToken = tokens.nextToken();
-    		if (nextToken == "=") {
-    			variables.put(variableName, nextInt());
-    		}
-    		else {
-    			System.out.println(token);
-    		}
-    		// TO DO
+    		lastVariable = token;
     		break;
     	}
     	return token;
     }
+	
+	private static String runToken(String token) {
+		return runToken(token, tokens);
+	}
     
     public static void translateGrammar(FileReader file) {
     	turtle = new DrawableTurtle();
@@ -92,9 +124,10 @@ public class TurtleSoup2 {
     	System.out.println(fileToString);
 		tokens = new StringTokenizer(fileToString);
 		String lastToken = "";
-		while(lastToken != "pro") {
-			lastToken = runToken(tokens.nextToken());
+		while(lastToken.equals("programEnd")) {
+			lastToken = runToken(nextToken());
     	}
+		scan.close();
     }
 
     /**
@@ -110,6 +143,21 @@ public class TurtleSoup2 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		translateGrammar(file);
+		try {
+			translateGrammar(file);
+		} catch (Exception e) {
+			System.out.println("Token number: " + tokenCounter + " Last Token: " + lastTokenEver);
+			String outStr = "Next tokens = ";
+			while(tokens.hasMoreTokens()) {
+				outStr = outStr + " " + tokens.nextToken();
+			}
+			System.out.println(outStr);
+			System.out.println(variables);
+			e.printStackTrace();
+			
+			// TODO: handle exception
+		}
+		
+		turtle.draw();
     }
 }
